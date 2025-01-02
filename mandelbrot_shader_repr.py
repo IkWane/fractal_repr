7388d4a067ad63123643e61f5cecc836766434ed
IkWane <raphounet78@gmail.com>
2025-01-02 19:19:14 +0100

    now with coloration and you can zoom and move around.

--- mandelbrot_shader_repr.py
+++ mandelbrot_shader_repr.py
@@ -1,7 +1,7 @@
 import pygame as pg
 import pygame_shaders as pgs
 import sys
-from math import sqrt
+from math import sqrt, exp
 
 def func(z, c) :
     return z**2 + c
@@ -13,11 +13,17 @@ screen = pg.display.set_mode(win_size, pg.OPENGL | pg.DOUBLEBUF | pg.HWSURFACE)
 display = pg.Surface(win_size)
 display.set_colorkey((0, 0, 0))
 center = size/2
-fractal_scale = 2
+fractal_scale = 4
 iterations = 10
 clock = pg.time.Clock()
 square_check = False
 check_distance = 5
+inputs = [0, 0, 0]
+constant = (0,0)
+offset = (0, 0)
+prev_mp = (0, 0)
+
+zoom = 0
 
 shader = pgs.Shader(
     win_size, win_size, 
@@ -29,6 +35,7 @@ shader = pgs.Shader(
 
 while 1 :
     clock.tick()
+    mp = pg.mouse.get_pos()
     for event in pg.event.get() :
         if (
             event.type == pg.QUIT or 
@@ -37,18 +44,47 @@ while 1 :
              ) :
             pg.quit()
             sys.exit()
-        elif event.type == pg.MOUSEWHEEL :
-            if iterations + event.y > 1 :
-                iterations += event.y
         elif event.type == pg.KEYDOWN :
             if event.key == pg.K_k :
                 square_check = False if square_check else True
+            elif event.key == pg.K_LCTRL :
+                inputs[0] = True
+            elif event.key == pg.K_LSHIFT :
+                inputs[2] = True
+        elif event.type == pg.KEYUP :
+            if event.key == pg.K_LCTRL :
+                inputs[0] = False
+            elif event.key == pg.K_LSHIFT :
+                inputs[2] = False
+        elif event.type == pg.MOUSEBUTTONDOWN :
+            if event.button == 1 :
+                inputs[1] = True
+        elif event.type == pg.MOUSEBUTTONUP :
+            if event.button == 1 :
+                inputs[1] = False
+        elif event.type == pg.MOUSEWHEEL :
+            if inputs[2] :
+                if iterations + event.y > 1 :
+                    iterations += event.y
+            else :
+                zoom += event.y
+                zoom = max(0, zoom)
+                fractal_scale = 4 * exp(-0.5 * zoom)
     
-    mp = pg.mouse.get_pos()
+    pgs.clear((0, 0, 0))
+    delta = (mp[0] - prev_mp[0], mp[1] - prev_mp[1])
+    if inputs[1] :
+        offset = ((offset[0] - delta[0] / size * fractal_scale), (offset[1] + delta[1] / size * fractal_scale))
     shader.send("iterations", [iterations])
     shader.send("checkDistance", [check_distance])
-    pgs.clear((0, 0, 0))
+    shader.send("constant", [(constant[0] / size - 0.5) * 4, (constant[1] / size - 0.5) * 4])
+    shader.send("offset", [offset[0], offset[1]])
+    shader.send("fractalScale", [fractal_scale])
     display.fill((0, 0, 0))
-    pg.display.set_caption(f"fps: {clock.get_fps():.2f} {iterations}") 
+    pg.display.set_caption(f"fps: {clock.get_fps():.2f} {iterations} {fractal_scale}") 
     shader.render(display)
+    if inputs[0] :
+        constant = mp
+        pg.draw.line(screen, (1.0, 1.0, 1.0, 1.0), (size/2,size/2), mp)
+    prev_mp = mp
     pg.display.flip()
--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -6,19 +6,32 @@ out vec4 color;
 
 uniform float iterations;
 uniform float checkDistance;
+uniform vec2 constant;
+uniform vec2 offset;
+uniform float fractalScale;
+
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
 
 void main() {
-    float fractalScale = 4;
-    vec2 current = vec2(0, 0);
     int total_iterations = 0;
-    vec2 coord = (fragmentTexCoord - 0.5) * fractalScale;
+    vec2 coord = (fragmentTexCoord - 0.5) * fractalScale + offset;
+    vec2 current = coord;
     for(int i = 0; i < int(iterations); i++) {
-        current = vec2(current.x * current.x - current.y * current.y, 2 * current.x * current.y) + coord;
+        current = vec2(current.x * current.x - current.y * current.y, 2 * current.x * current.y) + constant;
         if (length(current) > checkDistance) {
             total_iterations = i;
             break;
         }
     }
-    float c = total_iterations / iterations;
-    color = vec4(c, c, c, 1.0);
+    float intensity = total_iterations / iterations;
+    vec3 c = vec3(0.0);
+    if (intensity != 0) {
+        c = hsv2rgb(vec3(1.0-intensity, 1.0, 1.0));
+    }
+    color = vec4(c, 1.0);
 }
